ee859d912b2c2af51e995278ce221d0dce59185d
withme12 <1760057692@qq.com>
2019-06-10 17:59:31 +0800

    （测试更新）更新了Customer的查询全部service

--- src/main/java/com/briup/apps/ej/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/briup/apps/ej/service/impl/CustomerServiceImpl.java
@@ -1,8 +1,9 @@
-package com.briup.apps.ej.service;
+package com.briup.apps.ej.service.impl;
 
 import com.briup.apps.ej.bean.Customer;
 import com.briup.apps.ej.bean.CustomerExample;
 import com.briup.apps.ej.dao.CustomerMapper;
+import com.briup.apps.ej.service.ICustomerService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -17,5 +18,27 @@ public class CustomerServiceImpl implements ICustomerService {
     public List<Customer> findAll() {
         CustomerExample example=new CustomerExample();
         return customerMapper.selectByExample(example);
+    }
+
+    @Override
+    public Customer findCustomerById(Long id) {
+
+
+        return customerMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void insertOrUpdate(Customer Customer) throws Exception {
+
+    }
+
+    @Override
+    public void deleteCustomerById(Long id) throws Exception {
+
+    }
+
+    @Override
+    public List<Customer> query(Customer Customer) {
+        return null;
     }
 }

f42d9c85d04a0a772cd952eb8dc754d2041598a2
TSong1 <524396029@qq.com>
2019-06-13 10:34:36 +0800

    Order Order_line 的增删改查，批量删除

---
+++ src/main/java/com/briup/apps/ej/service/IOrder_lineService.java
@@ -0,0 +1,19 @@
+package com.briup.apps.ej.service;
+
+import com.briup.apps.ej.bean.Order;
+import com.briup.apps.ej.bean.Order_line;
+
+import java.util.List;
+
+public interface IOrder_lineService {
+//    int deleteByPrimaryKey(Long id);
+//    int updateByPrimaryKey(Order_line record);
+//    int insert(Order_line record);
+//    Order_line selectByPrimaryKey(Long id);
+
+    List<Order_line> findAll();
+    Order_line findOrder_lineById(Long id);
+    void insertOrUpdate(Order_line order_line)throws Exception;
+    void deleteOrder_lineById(Long id) throws Exception;
+    void batchDelete(long[] ids) throws Exception;
+}
--- src/main/java/com/briup/apps/ej/web/controller/Order_lineController.java
+++ src/main/java/com/briup/apps/ej/web/controller/Order_lineController.java
@@ -2,8 +2,7 @@ package com.briup.apps.ej.web.controller;
 
 import com.briup.apps.ej.bean.Order;
 import com.briup.apps.ej.bean.Order_line;
-import com.briup.apps.ej.service.IOrder;
-import com.briup.apps.ej.service.IOrder_line;
+import com.briup.apps.ej.service.IOrder_lineService;
 import com.briup.apps.ej.utils.Message;
 import com.briup.apps.ej.utils.MessageUtil;
 import io.swagger.annotations.ApiOperation;
@@ -14,23 +13,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/order_line")
 public class Order_lineController {
 
     @Autowired
-    private IOrder_line order_line;
-    @ApiOperation("模糊查询")
+    private IOrder_lineService order_lineService;
+
+    @GetMapping("findAll")
+    public Message findAll(){
+        List<Order_line> list = order_lineService.findAll();
+        return MessageUtil.success("success",list);
+    }
+
+    @ApiOperation("通过id查询")
     @GetMapping("findById")
-    public Message findById(@ApiParam(value = "主键",required = true) @RequestParam(value = "id") long id){
-        return MessageUtil.success("success",order_line.selectByPrimaryKey(id));
+    public Message findById(
+            @ApiParam(value = "主键",required = true)
+            @RequestParam(value = "id") long id){
+        return MessageUtil.success("success",order_lineService.findOrder_lineById(id));
     }
 
     @ApiOperation("通过id删除用户")
     @GetMapping("deleteById")
     public Message deleteById(long id){
         try {
-            order_line.deleteByPrimaryKey(id);
+            order_lineService.deleteOrder_lineById(id);
             return MessageUtil.success("删除成功!");
         } catch (Exception e) {
             e.printStackTrace();
@@ -38,10 +48,22 @@ public class Order_lineController {
         }
     }
 
-    
-    @ApiOperation("插入")
-    @GetMapping("insertSelective")
-    public Message insert(Order_line record){
-        return MessageUtil.success("success",order_line.insert(record));
+    @ApiOperation("批量删除")
+    @GetMapping("batchDelete")
+    public Message batchDelete(long[] ids) throws Exception{
+        order_lineService.batchDelete(ids);
+        return MessageUtil.success("9批量删除成功");
+    }
+
+    @ApiOperation("保存或更新用户信息")
+    @GetMapping("saveOrUpdate")
+    public Message insertOrUpdate(Order_line order_line) {
+        try {
+            order_lineService.insertOrUpdate(order_line);
+            return MessageUtil.success("保存成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return MessageUtil.error(e.getMessage());
+        }
     }
 }
